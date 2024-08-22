c47ce1385d630ff311edc01d160ba66b689d9097
Sam <asamadans@gmail.com>
2024-08-22 23:18:55 +0530

    working

---
+++ programs/anchor-movie-review-program/src/lib.rs
@@ -0,0 +1,138 @@
+use anchor_lang::prelude::*;
+
+declare_id!("6SKSBEvpnZVjNGs3LfecJJXoZv1PuRQ81i2LT6vgN7GB");
+
+#[program]
+pub mod anchor_movie_review_program {
+    use super::*;
+
+    pub fn add_movie_review(
+        ctx: Context<AddMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        require!(rating >= 1 && rating <= 5, MovieReviewError::InvalidRating);
+
+        require!(title.len() <= 20, MovieReviewError::TitleTooLong);
+
+        require!(
+            description.len() <= 50,
+            MovieReviewError::DescriptionTooLong
+        );
+
+        msg!("Movie Review Account Created");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
+
+        let movie_review = &mut ctx.accounts.movie_review;
+        movie_review.reviewer = ctx.accounts.initializer.key();
+        movie_review.title = title;
+        movie_review.rating = rating;
+        movie_review.description = description;
+        Ok(())
+    }
+
+    pub fn update_movie_review(
+        ctx: Context<UpdateMovieReview>,
+        title: String,
+        description: String,
+        rating: u8,
+    ) -> Result<()> {
+        msg!("Movie review account space reallocated");
+        msg!("Title: {}", title);
+        msg!("Description: {}", description);
+        msg!("Rating: {}", rating);
+
+        let movie_review = &mut ctx.accounts.movie_review;
+        movie_review.rating = rating;
+        movie_review.description = description;
+
+        Ok(())
+    }
+
+    pub fn delete_movie_review(_ctx: Context<DeleteMovieReview>, title: String) -> Result<()> {
+        msg!("Movie review for {} deleted", title);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+#[instruction(title:String, description:String)]
+pub struct AddMovieReview<'info> {
+    #[account(
+        init,
+        seeds = [title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        payer = initializer,
+        space = MovieAccountState::INIT_SPACE + title.len() + description.len()
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title:String, description:String)]
+pub struct UpdateMovieReview<'info> {
+    #[account(
+        mut,
+        seeds = [title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        realloc = MovieAccountState::INIT_SPACE + title.len() + description.len(),
+        realloc::payer = initializer,
+        realloc::zero = true,
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(title: String)]
+pub struct DeleteMovieReview<'info> {
+    #[account(
+        mut,
+        seeds=[title.as_bytes(), initializer.key().as_ref()],
+        bump,
+        close=initializer
+    )]
+    pub movie_review: Account<'info, MovieAccountState>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+pub struct MovieAccountState {
+    pub reviewer: Pubkey,    // 32
+    pub rating: u8,          // 1
+    pub title: String,       // 4 + len()
+    pub description: String, // 4 + len()
+}
+
+const DISCRIMINATOR_LENGTH: usize = 8;
+const PUBLIC_KEY_LENGTH: usize = 32;
+const U8_LENGTH: usize = 1;
+const STRING_LENGTH_PREFIX: usize = 4; // Stores the length of the string
+
+impl Space for MovieAccountState {
+    const INIT_SPACE: usize = DISCRIMINATOR_LENGTH
+        + PUBLIC_KEY_LENGTH  // reviewer
+        + U8_LENGTH  // rating
+        + STRING_LENGTH_PREFIX  // title length
+        + STRING_LENGTH_PREFIX; // description length
+}
+
+#[error_code]
+enum MovieReviewError {
+    #[msg("Rating must be between 1 and 5")]
+    InvalidRating,
+    #[msg("Movie Title too long")]
+    TitleTooLong,
+    #[msg("Movie Description too long")]
+    DescriptionTooLong,
+}
